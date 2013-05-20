961bc2922ec8fe1d96382dc20712b17554c0cf49
Chris Jobling <C.P.Jobling@Swansea.ac.uk>
2013-05-20 16:07:18 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proman2014::Application.config.secret_token = '4514a6dd7f278063cb3b2ed121421a48bcfd6c292d3443b252456003bee8488fc06f3c678bd560b7f2f672cb7c7488902e8a4a1a7c60f19ed3ca5a7372d284de'
